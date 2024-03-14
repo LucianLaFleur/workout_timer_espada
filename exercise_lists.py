c099cc98565fc7a211ba65304f8b838f2d37e3ba
John K. Summerlin <37386570+LucianLaFleur@users.noreply.github.com>
2024-03-14 12:28:53 -0400

    Create exercise_lists.py
    - added abs list (single_motion and dual_motion) as the current sample
    - each list is labeled with the equipment type, such as bodyweight or dumbbells, and then the target muscle group, then if it's a mirrored exercise or a single one.
    ex. bodyweight abs singles --> only bodyweight is needed, targets abs, exercises are not mirrored (so stand alone, opo. Left/Right as needed for mirrored moves)
    
    -Single_motion lists are lists of strings indicating exercise moves. They are balanced or alternating, so it they facilitate uniform muscle development, at the cost of less focus on either side.
    
    - Dual_motion lists will always have sub-arrays of 2 string items in length, usually left-right versions of a move. Greater focus on one side at a time means greater intensity, at the cost of time and needing to flip positions.
    
    - Trio motions do not yet exist, but might have a niche purpose in future.
    
    - Initial commit, many commented-out exercises are unreviewed, but exist for future development

---
+++ exercise_lists.py
@@ -0,0 +1,279 @@
+
+# ------------------------------------------------------------------------------------------------------------------------------------
+# EXERCISE MOVE Collection:
+
+# STATUS:
+    # - Exercises need review so compounds can be generated for whichever.
+    # -- compound/specials should get their own arr
+
+# ------------------------------------------------------------------------------------------------------------------------------------ 
+# ---- UNSORTED / Advanced oddities
+# ------------------------------------------------------------------------------------------------------------------------------------ 
+# ADV: spinning crab compound
+    #  Down on crouch to T-plank, and crabwalk pose; Action, side T-plank, rotate around into lunge down-position, step up; you'll be facing the opposite way of where you're looking in crab position.
+# gollum shuffle, left and right. gollum shuffle, triangle pattern
+# Floor jack pike : push-up position, then legs spread, together, tuck in, then back to normal push-up position
+
+# side to side roll/sway evades?
+# side to side bouncing hops, side to side pike jumps
+
+# curtsey squats
+# "leg work, light":
+#         "marching zombie high knees", "marching doll high knees", "standing bird-dog reach", "standing single leg hip circles"
+
+#     "leg work, hard":
+#         "triangle pose side lunges", "step-back lunges", "three-point frog squats", "prisoner squat to standing bicycle"
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------
+#  Bodyweight only / no equipment needed
+# ------------------------------------------------------------------------------------------------------------------------------------ 
+
+# !! update to array format and put mirrored exercises in pairs
+bodyweight_abs_single_motion_list = [
+    # exceptional focus on the core and on balanced footwork
+    "Slow tension standing bicycles", 
+    "windmill toe touches",
+    "windmill sky chops", 
+    "stellar windmills",
+    "belly dancer kicks",
+    # often done with weights or a medicine ball...
+    "torso twist, alternating lunges",
+    "squat between standing bicycles",
+    # knee-tuck extensions, which is the single-side version of the standing bicycle as opposed to bicycles being opposite side, upper abs target
+    "straight-leg toe taps",
+    # legs straight, torso twist, but arms are up to assist with opening abs and targeting lower abdominals
+    "draw sword, side-bend",
+    # "the shy schoolgirl", single calf raise to pointed toe with the torso twisting
+    "Torso twist hip taps",
+    "Open hip, straight high-knees",
+    # like you're grabbing a dude's head and driving it into your knee
+    "Cross-core knee strike",
+    # ADVANCED! No tension on back, slow motion, hold leg out and strengthen pelvis
+    # "arm support, side-leg raise",
+    # advanced 180 degree motion
+    # "Arm support, sweeping left leg raise",
+    # "Arm support, sweeping right leg raise",
+    # "Lying left leg raise",
+    # "Lying right leg raise"
+    # opposite calf raise to reaching arm, tension !! not sluggers
+    "Torso twisting slow side punches",
+    # karate punch to side, then off-hand arm in chamber position, watch footwork for firm planting
+    "Lighthouse torso twists",
+    # "boxer's forward lean, back dodge cross"
+    # "taunting sway cross",
+    # "arms akimbo alternating side-bend",
+    # "leg lifts",
+    # "center row",
+    # "center row, leg lift combo",
+]
+
+bodyweight_abs_dual_motion_list = [
+    # lower abs
+    ["left lying hip-circles", "right lying hip circles"],
+    # Knee tuck focuses on lower abs from a standing position
+    ["left out-step knee strike", "right out-step knee strike"],
+# Obliques
+    ["left side bends", "right side bends"],
+    
+    # big arm circle, little torso movement
+    # "back-stroke torso twists",
+    # "right arm circle torso rotations",
+    # full extension version of the petty bicycle
+    # left three-quarters standing side-bend, cross
+    # "Left/right rock-and-box", 
+    # "right rock-and-box",
+    # keep hips open, knee forward, pull in with the abs, open with the hips
+    ["left out-step sky-punch knee-tuck","right out-step sky-punch knee-tuck"],
+    # "right sky-punch knee-tuck"
+    # tree chopper sideways motion # kek, if it's a busty tomboy wearing a low tank top, it's great cleavage torso twists
+    # "Left/right great cleaver torso twists",
+    # focus on balance, slightly forward with hips and back, but don't go ham with the pelvic thrust, your back should only bend slightly and not strain, keep eyes forward, do not look down-up at the same time... throws off balance
+    # "heel toe rockers",
+    #
+]
+#  NOT standing-friendly abs -------------------------------------
+    # "full extension starfish crunch",
+    # "crab pose, half moon kicks", 
+    # "seated scissor circles",
+    # "left side-lying knee-to-elbow",
+    # "right side-lying knee-to-elbow",
+    # ADVANCED: "the venus fly trap tap", pull with obliques, side should not be hurting
+    # "left side, leg-extended v-ups",
+    # "right side, leg-extended v-ups",
+    # "seated flutter kicks",
+    #  reverse sphynx leg raise,
+    # sitting leg lever # small range of motion leg raise, "the paperclip"
+    # "basic upper abs crunch"
+    # "tension scissor circles"
+    # "left seated hip abduction", "right seated hip abduction"
+# ------------------------------------------------------------------
+
+# abductors and glutes:
+    # basic side lunges
+    # side lunge cross punches #horse stance, low dodge, oblique for punch tension
+    # side lunge windmills (stay low with the wide stance, good for thighs)
+    # lean forward, kick back
+
+#  Raw abs:
+# kneeling back lean
+# punch down torso-twists # the shy schoolgirl
+# "wall-crawlers",
+# "Side-chamber slugs", # torso twist side-punches
+# "Left/right arm support, sweeping leg raise",
+# "squat axe handle uppercut"
+
+# Abs and arms, generally
+# "knuckle-pecks"
+
+# abs and quad: 
+# "kneeling back lean (tension)", 
+# "alternating high-knees", 
+# "hopping high knees", 
+# "elbow to knee, squat isometric"
+
+# abs and hams :
+# "squat between standing bicycles", # standing bicycle, but after doing each side, do a squat between
+
+# ankles/calves: "ankle hops", 
+# "squat to ankle raise" # keep butt out or it'll hurt your back, tighten core, arms out front for balance, should feel in top of leg
+# ["Seated knee up toe-point, heel point", "seated boat isometric, toe-point heel-point"]
+
+# biceps:
+# "curl to overhead press", 
+# "biceb curl fly press", 
+# "curl up, double punch out" 
+# "mummy hug" # zombie curl, arms across chest, outward fly, clap in, z-curl down
+# "T-rex hammer curl", #wrists bent to add extra tension and activate forearms 
+# "claw-hand z-curl", 
+# "claw hand alternating bicep curls", 
+# "barrel hug hammer curl", 
+# "sky scratchers" # "Volkenkratzer", claw pose biceb curl with mulitary press
+# Scary bear bicebs # claw pose biceb curl, arm rotation 
+# "45-degree palm press" # the bell-ringer
+# seated hip-up bicep curl # AT-field curls, because zettai ryouiki
+
+# forearms and wrists: 
+    # ["arms out forward, fin flap", "arms out to sides, fin flap"] #   also called the feline paw-flap and fairy/penguin flap
+    # ["arms out forward, fin flap", "arms out to sides, fin flap"]
+    # ["behind-the-back wrist tensions", "A-pose inner wrist curls"],
+    # ["A-pose wrist hammer curl up", "A-pose wrist hammer curl up"]
+    # "alternating claw-hand forearm raise", # same as zombie claw forearm raise, looks like frankenstein's monster
+    # "zombie claw forearm raise"
+    # "T-rex bicep curl", #wrists bent to add extra tension and activate forearms 
+    # "A-pose rotating claw-curls", #forearm tension
+    # "claw-hand Z-curls",
+    # "arms forward, tension wrist rotations",
+    # "arms out, tension wrist rotations" # crossover with shoulders  
+    # ["Cat's claw wrist bends", "thriller-pose wrist circles"], # side to side doing the car pose, forearm stretch ! be sure to go all the way up and all the way down with a wrist flap at the peak of each thriller pose
+        
+# hams: 
+# "side-stepping lunges",  Step out, transfer weight, slide up (maybe a bit advanced?)
+# "sumo palm-strike squats (palm strike at the bottom and top, left, left, then right right on next rep)"
+# REJECTED: front and back leap-frogs.
+
+# hamstring and shoulders :
+#  "squat to overhead press" # the superhero lift
+#   "sunrise squats (arm reach upward)",
+
+# hip mobility: 
+#4-count body-bows, clockwise/counter # (nickel nickel quarter dollar), 
+# standing hula hip rotations
+# standing hip abduction
+# side-lying hip abduction
+
+# oblique: 
+# "seiza to kneeling warrior, alternating arms"
+# "Spiderman push-up"
+
+# quads, general: "alternating kneeling warrior pose lunges", "squat pulse", "sumo stomp squats"
+# ADV: crouch walk: crouch walk, stand up, keep feet shoulder-width apart, bend your knees, feet together before standing up, do not put pressure on your back, you may need to bend lower to do so
+# twist jacks # alternating lunges torso twists
+# Ice skater lunges # torso twists, leg goes back instead of hopping in place with a twist-jack
+# Sasquatch squat walk, arms sway to the side, two step forward, legs together, alternate leg (planting one foot at a time), always twisting toward the knee that's forward, if exhausted, can rest on that knee and push yourself up from it
+
+#quad and ham:
+#  squat-between stepping lunges
+# alternating 3/4 frog squats
+# "squat pulse stand-up"
+
+
+# pecs: 
+# closet door open-and-shut 
+        # "Do the monkey!",
+        # "knuckle-pecks" # alternate arms with one arm in cactus position and the other with knuckles against shoulder like knocking on a door where the pectoral meets the armpit; something of a monkey-move, also hits side abs if additional motion added
+        # "The Pharoh's Salute" # x across chest 
+        # "sky-punch fly press",
+        # "biceb curl fly press", 
+        # "single arm fly side-steps",
+        # "arms fly out, double-punch forward", 
+        # "hands-together up-and-downs", 
+        # hands together, press up like hands together in tree pose, then down, like a lateral row
+        # "open hip thunderbolt palm press"
+         # kneeling version lets you back bend for more range of movement
+        # kneeling thunderbolt prayer,
+        # "X-Y fly press" # fly press, cross arms in an X, then arms go up to a y-pose, back in for X
+        # *shut the door / venus flytrap = pecs
+        # *venus flytrap palm press pull # a "gathering in" motion focusing on pec tension
+        #  Spiderman push-up
+    
+# shoulders
+# *wax on, wax off
+#  left diag, right diag, out to sides while bending back, arms swing forward, pump both arms up
+                    #  On diagonals the opposite leg steps out for support
+#  "four-count cheer squad",
+# "single arm side-stepping sky punches", 
+# "sky-punch, side stab",
+# "ankle raise, shoulder shrugs",
+# "ankle raise, big shoulder circles"
+# "slow overhead lat pull-down"
+# "overhead lat pull down push-out"
+# "lateral pull down to shoulder shrug forward-back",
+
+# back:
+# *closet door open-and-shut 
+# alternating back-stroke arm rotations
+# double back-stroke arm rotations
+
+#  Triceps:
+# "left overhead tricep tomahawk", "right overhead tricep tomahawk"
+# "left tension tricep kickbacks", "right tension tricep kickbacks"
+# ["side-step, side-stab"]
+# ["alternating hammer-downs (triceps)", "double-fang downward stabs"]
+# "bodyweight tricep dips"
+# "Yawnining tricep extension" # chicken wing position, but palms out, above shouldersarms are going up against gravity, bending at the elbow
+# "ice cliff climber" #focus on tricep tension, like an arm bicycle doing a climbing motion as if having daggers in reverse grip and climbing up a wall
+# *Needs name: upper chicken wing position, over chop, under chop
+
+# !! wildcard extra compounds:
+# "vampire wings"# from arms at sides, zombie claw up (for forearm tension), upward fly like opening a cape, arms rake down, picep curl inward, z-curl down to starting position
+# *["3-count, X, Y, T, wrist rotations"], # "XYT, or "exit" dancer's wrist rotations, arms out, hug in for x, rotating wrists with each move                                                     
+# *["5-count yak's tax, wrist rotations"] # ADV. "Yak's Tax" means Y-A-X-T-X
+
+
+# ------------------- all these need names
+# upper chicken wing position, over chop, under chop
+# ["3-count, X, Y, T, wrist rotations"], # "XYT, or "exit" dancer's wrist rotations, arms out, hug in for x, rotating wrists with each move                                                     
+# ["5-count yak's tax, wrist rotations"] # ADV. "Yak's Tax" means Y-A-X-T-X
+#  closet door open-and-shut 
+
+# palm press offering / palm press give and take
+# Water wheel palm press # imagine a big wheel that you have to close your palms over and rotate in a circle back and forth
+#  open the door = back workout
+# shut the door / venus flytrap = pecs
+# venus flytrap palm press pull # a "gathering in" motion focusing on pec tension
+# X - form, victory pose y-arms, then dorn to a-pose hammer tricep extension
+# The saggitarius # alternating 45 degree archer pulls
+# Basic archer pulls L/R
+# gorilla archers # archer pull, but on draw, supporting hand comes in for a chest thump, then alternate sides
+# Archer pull to side-knife stab # pulling arm pulls and holds, "bow-holding" hand comes in to chest
+    # ^^ no irl analogue, so it's like taunting a goblin to come close since you're using a bow, but the bow is an illusion and it's actually a knife, so you ready up to hit them properly
+# side to side upward arcing tricep extension
+# arms out, torso punch to the side and pull, tricep arc, then twist and punch with other hand
+
+# ... misc, unsorted exercises
+# heel press row
+#  single leg RDL
+#  forward-leaning RDL
+# scissor lotus abduction # v-sit with legs out, brings legs in for indian-style sit, then out to V again, alternating which leg crosses in on top. Tap opposite heel to hamstring on inner position, V sit is outer position.
+#   --  AKA -> criss-cross applesauce # overly possessive girlfriend secret inner thigh no-jutsu
